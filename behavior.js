0e2258666f91b17578429d2f473d1f53557d58d0
PrincessOkafor <80207458+PrincessOkafor@users.noreply.github.com>
2021-06-25 11:23:06 +0100

    Add files via upload

---
+++ behavior.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    var thumbnailElement = document.getElementById("smart_thumbnail");
+    thumbnailElement.className = "";
+    if (thumbnailElement.className == "") {
+
+    }
+});
